d1eddba513c89eb3451154ffb3cc9ecd460cd444
Sathwik-RS <rssathwik5@gmail.com>
2025-11-04 19:35:45 +0530

    hello change

--- .dist/Drawing.py
+++ .dist/Drawing.py
@@ -5,7 +5,7 @@ blank=np.zeros((500,500,3), dtype='uint8')
 blank[:] =0,0,0
 cv.imshow('blank image',blank)
 #apply to specific spots
-blank[0:0,500:500] =190,200,190
+blank[0:0,300:300] =190,200,190
 cv.imshow('blank image',blank)
 rect=cv.rectangle(blank,(0,0),(250,250),(0,255,0),thickness=2)
 cv.imshow('rectanglen image',rect)
